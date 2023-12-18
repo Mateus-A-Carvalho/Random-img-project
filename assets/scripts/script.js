09380b1154f0350deab1bdb04b050c144a6c969b
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-12-18 11:14:01 -0300

    Create Element Functionality - DONE I create a function that receive an element as parameter, add this element in mainSectonEl and set the attribute 'src' with link of https://picsum.photos/ to generate a random picture. Now, remain the functionality that create more than one elements that I will use when browser load and click in button.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,11 @@
-alert('The script is running!')
+const mainSectionEl = document.querySelector('#main-content');
+const btnEl = document.querySelector('#btn');
+const elementCreated = document.createElement('img');
+
+
+const generatePicture = (element) => {
+    mainSectionEl.appendChild(element);
+    element.setAttribute("src", "https://picsum.photos/300");
+}
+
+generatePicture(elementCreated);
