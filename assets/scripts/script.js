2edd6e7eae08905a3688b929029648cb0b722d76
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2023-12-20 21:02:25 -0300

    Random Generate Pictures Functionality - DONE The function accountable to generate pictures, randomly works this way: 1 - Create a random number using the Math object, between 0 and 1000; 2 - Create an img element; 3 - Set a 'src' attribute to this element containing the link to the random picture, using the randomly number created before to pass any parameter, to prevent the brownser make cash of links. This approach prevents two equal imgs; 4 - Add this element in the mainSectionEl;
    The next functionality is buttonClickGenerate(), that I just added the generatePicture() function inside an Event Listener.
    
    Now, I will start the styling of this project.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,25 @@
 const mainSectionEl = document.querySelector('#main-content');
-const btnEl = document.querySelector('#btn');
-const elementCreated = document.createElement('img');
+const btnEl = document.querySelector('#btn');    
 
-
-const generatePicture = (element) => {
-    mainSectionEl.appendChild(element);
-    element.setAttribute("src", "https://picsum.photos/300");
+const generatePicture = () => {
+    const imgRandomParameter = Math.floor(Math.random() * 1000);    
+    const elementCreated = document.createElement('img');
+    elementCreated.setAttribute("src", `https://picsum.photos/300?r=${imgRandomParameter}`);
+    mainSectionEl.appendChild(elementCreated);
 }
 
-generatePicture(elementCreated);
+const buttonClickGenerate = (btn) => {
+    btn.addEventListener("click", () => {
+        
+        generatePicture();
+        generatePicture();
+        generatePicture();
+        generatePicture();        
+    })
+}
+
+generatePicture();
+generatePicture();
+generatePicture();
+generatePicture();
+buttonClickGenerate(btnEl);
