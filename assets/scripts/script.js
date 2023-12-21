82a1f7f4f9632a49a8579322ea07526af9d685f1
Mateus-A-Carvalho <mateusabreu94@hotmail.com>
2023-12-21 05:48:02 -0300

    STYLING - STARTED Organizing the layout of elements.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,11 @@
-const mainSectionEl = document.querySelector('#main-content');
 const btnEl = document.querySelector('#btn');    
+const imgWrapperEl = document.querySelector('#img-wrapper');
 
 const generatePicture = () => {
     const imgRandomParameter = Math.floor(Math.random() * 1000);    
     const elementCreated = document.createElement('img');
     elementCreated.setAttribute("src", `https://picsum.photos/300?r=${imgRandomParameter}`);
-    mainSectionEl.appendChild(elementCreated);
+    imgWrapperEl.appendChild(elementCreated);
 }
 
 const buttonClickGenerate = (btn) => {
@@ -18,6 +18,8 @@ const buttonClickGenerate = (btn) => {
     })
 }
 
+generatePicture();
+generatePicture();
 generatePicture();
 generatePicture();
 generatePicture();
